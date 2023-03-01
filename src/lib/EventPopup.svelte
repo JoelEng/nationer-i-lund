cb3d2dcc86f756d70b3156e23ce2d72d6db81df1
JoelEngstrom <joel.n.engstrom@protonmail.com>
2023-03-01 14:59:03 +0100

    added booking to events

--- src/lib/EventPopup.svelte
+++ src/lib/EventPopup.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Modal, Button, ButtonGroup, A } from 'flowbite-svelte';
+	import { Modal, Button, ButtonGroup, A, Input, Label } from 'flowbite-svelte';
 	import Heart from './Heart.svelte';
 	import type { Event } from './types';
 
@@ -38,10 +38,34 @@
 				href={`https://www.google.com/maps/search/?api=1&query=${location}`}>{location}</A
 			>
 		</p>
+
+		<form class="flex flex-col">
+			<div class="grid gap-6 mb-6 md:grid-cols-2">
+				<div>
+					<Label for="first_name">Förnamn</Label>
+					<Input type="text" id="first_name" placeholder="Joel" required />
+				</div>
+
+				<div>
+					<Label for="last_name">Efternamn</Label>
+					<Input type="text" id="last_name" placeholder="Joelsson" required />
+				</div>
+
+				<div>
+					<Label for="email">Emejl</Label>
+					<Input type="email" id="email" placeholder="joel@joel.se" required />
+				</div>
+
+				<div>
+					<Label for="tel">Telefonnummer</Label>
+					<Input type="tel" id="tel" placeholder="0703180710" required />
+				</div>
+			</div>
+			<Button type="submit" color="blue">Boka</Button>
+		</form>
 	</svelte:fragment>
 	<div slot="footer" class="flex w-full justify-between">
 		<ButtonGroup class="space-x-px">
-			<Button color="blue">Boka</Button>
 			<Button color="light" href="mailto:{email}">Kontakta oss</Button>
 		</ButtonGroup>
 		<Heart size={10} {event} />
