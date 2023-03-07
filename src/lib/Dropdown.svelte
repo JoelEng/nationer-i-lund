2d2d593f73a0aafa4aa45d6852e462ab545c6928
FelixApellSkjutar <felix.apellskjutar@gmail.com>
2023-03-07 20:19:11 +0100

    Clean up, removing duplicated code.

--- src/lib/Dropdown.svelte
+++ src/lib/Dropdown.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { Button, Dropdown, DropdownItem, Chevron, Checkbox } from 'flowbite-svelte';
+	import { Button, Dropdown, Chevron, Checkbox } from 'flowbite-svelte';
 	import {selectedOrganizers, selectedTime} from './stores';
 
 	const times = ['Lunch', 'Eftermiddag', 'Kväll']
-	const orgs = ['Lunds Nation', 'Göteborgs Nation', 'Malmö Nation', 'Kalmar Nation', 'Helsingkrona Nation', 'Sydskånska Nationen', 'Wermlands Nation', 'Västgöta Nation', 'Östgöta Nation', 'Blekingska Nationen', 'Hallands Nation'];
+	const orgs = ['P6', 'Akademiska Föreningen','Boelspexarna', 'Kuratorskollegiet', 'Amnesty Lund Student Group', 'Samhällsvetarkåren vid Lunds universitet', 'Lunds Nation', 'Göteborgs Nation', 'Malmö Nation', 'Kalmar Nation', 'Helsingkrona Nation', 'Sydskånska Nationen', 'Wermlands Nation', 'Västgöta Nation', 'Östgöta Nation', 'Blekingska Nationen', 'Hallands Nation'];
 
 	const handletoggle = (e:Event) => {
 		//If the checkbox is checked, add it to the store
@@ -22,26 +22,13 @@
 		}
 	}
 
-	function isInList (org:string) {
-		if($selectedOrganizers.has(org)){
-			return true;
-		}
-		else return false;
-	}
-
-	function isInTimeList (time:string) {
-		if($selectedTime.has(time)){
-			return true;
-		}
-		else return false;
-	}
 </script>
 
 <Button color="light"><Chevron>Nationer</Chevron></Button>
 <Dropdown class="w-44 p-3 space-y-3 text-sm">
 	{#each orgs as org}
 		<li>
-			<Checkbox value={org} checked={isInList(org)} on:change={handletoggle}>{org}</Checkbox>
+			<Checkbox value={org} checked={$selectedOrganizers.has(org)} on:change={handletoggle}>{org}</Checkbox>
 		</li>
 	{/each}
 </Dropdown>
@@ -50,7 +37,7 @@
 <Dropdown class="w-44 p-3 space-y-3 text-sm">
 	{#each times as time}
 		<li>
-			<Checkbox value={time} checked={isInTimeList(time)} on:change={handleTimeToggle}>{time}</Checkbox>
+			<Checkbox value={time} checked={$selectedTime.has(time)} on:change={handleTimeToggle}>{time}</Checkbox>
 		</li>
 	{/each}
 </Dropdown>
