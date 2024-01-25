ebd1d420d505364eb9ee21aaa27e4a7f02336e64
Joel Engström <joel.n.engstrom@protonmail.com>
2024-01-25 23:58:30 +0100

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.postcss';
 	import Navbar from '$lib/Navbar.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <!-- <Navbar /> -->
